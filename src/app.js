02249ce770a8e9f56d5e207cd0fbf5cfecfbb93a
davis_chang <davis chang>
2018-10-10 14:09:42 +0800

    Promose, rxjs - from

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import $ from 'jquery';
-import { fromEvent, Observable, of } from 'rxjs';
+import { fromEvent, Observable, of, from } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 console.log('RxJS Practices!!');
-const btn = $('#btn');
-const input = $('#input');
-const output = $('#output');
 
+const btn = $('#btn');
 const btnStream$ = fromEvent(btn, 'click');
 btnStream$.subscribe(
   (event) => {
     console.log('clicked', event);
+    alert(JSON.stringify(event));
   },
   (error) => {
     console.log('error');
@@ -20,17 +19,20 @@ btnStream$.subscribe(
   }
 );
 
+const input = $('#input');
+const output = $('#output');
 const inputStream$ = fromEvent(input, 'keyup');
 inputStream$.subscribe(
   (event) => {
-    output.append(`keyup - ${event.target.value}`);
+    output.html(`<p>keyup: ${event.target.value}</p>`);
   },
 );
 
+const mouseOutput = $('#mouse_output');
 const moveStream$ = fromEvent(document, 'mousemove');
 moveStream$.subscribe(
   (event) => {
-    output.html(`mousemove - X: ${event.clientX}, Y: ${event.clientY}`);
+    mouseOutput.html(`mousemove - X: ${event.clientX}, Y: ${event.clientY}`);
   },
 );
 
@@ -63,11 +65,36 @@ source$
     }
   );
 
+/* Promose */
+const myPromise = new Promise((resolve, reject) => {
+  console.log('Create Promise');
+  setTimeout(() => {
+    resolve('Hello from Promise!');
+  }, 3000);
+})
+
+const getUserInfo = (username) => {
+  return $.ajax({
+    url: `https://api.github.com/users/${username}`,
+    dataType: 'jsonp',
+  }).promise();
+}
+getUserInfo('DavisChang').then(result => {
+  console.log('DavisChang result:', result);
+})
 
 
-
-
-
+fromEvent($('#promise_input'), 'keyup').subscribe(event => {
+  const userName = event.target.value;
+  /* just use from instead of fromPromise */
+  from(getUserInfo(userName))
+    .subscribe(result => {
+      $('#name').text(result.data.name);
+      $('#avatar_url').text(result.data.avatar_url);
+      $('#repos_url').text(result.data.repos_url);
+      $('#public_repos').text(result.data.public_repos);
+    });
+});
 
 
 
