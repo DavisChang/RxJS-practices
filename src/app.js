36dc7b18a59735c03797e7edc9d990a8b1fbabbf
davis_chang <davis chang>
2018-10-10 11:26:42 +0800

    fromEvent, Observable, catchError

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 console.log('RxJS Practices!!');
 const btn = $('#btn');
+const input = $('#input');
+const output = $('#output');
 
 const btnStream$ = fromEvent(btn, 'click');
-
 btnStream$.subscribe(
   (event) => {
     console.log('clicked', event);
@@ -17,3 +19,55 @@ btnStream$.subscribe(
     console.log('completed!');
   }
 );
+
+const inputStream$ = fromEvent(input, 'keyup');
+inputStream$.subscribe(
+  (event) => {
+    output.append(`keyup - ${event.target.value}`);
+  },
+);
+
+const moveStream$ = fromEvent(document, 'mousemove');
+moveStream$.subscribe(
+  (event) => {
+    output.html(`mousemove - X: ${event.clientX}, Y: ${event.clientY}`);
+  },
+);
+
+
+/* Observable */
+const source$ = Observable.create(observer => {
+  console.log('Observable');
+  observer.next('Hello');
+  observer.next('World');
+
+  observer.error(new Error('Error: Something went wrong!'));
+
+  setTimeout(() => {
+    observer.next('Yet another value');
+    observer.complete();
+  }, 3000);
+});
+
+source$
+  .pipe(catchError(val => of(`I caught: ${val}`)))
+  .subscribe(
+    x => {
+      console.log(x);
+    },
+    err => {
+      console.log('err:', err);
+    },
+    () => {
+      console.log('completed');
+    }
+  );
+
+
+
+
+
+
+
+
+
